dd67ed3265f4fe30519bcdb63fc2b662bf558593
Sandro Sauer <sauersandro149@gmail.com>
2022-07-30 22:42:07 +0200

    Update some node modules

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,4 +1,5 @@
-export function secondsFromHMS(hms: string) {
+export function secondsFromHMS(hms: string): number {
+  if (!hms || hms === '') return 0;
   const [hours, minutes, seconds] = hms.split(':');
   return (+hours) * 60 * 60 + (+minutes) * 60 + (+seconds);
 }
